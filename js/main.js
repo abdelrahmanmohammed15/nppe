6a1d178cea6cbdf59d88caeb524ced0a6cb21a9e
abdelrahmanmohammed15 <139029728+abdelrahmanmohammed15@users.noreply.github.com>
2023-07-09 06:30:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var typed = new Typed('#element', {
+  strings: ['Akram Mohamed', 
+            'Devloper',
+            'Designer'],
+            typeSpeed: 100,
+            loop: true,
+            loopCount: Infinity,
+            backSpeed: 50,
+            backDelay: 2500,
+
+});
